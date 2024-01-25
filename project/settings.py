6e0f84b535e13e4d95c1123b3971db8887c1121e
Prashanth <guntaprashanthkumar@gmail.com>
2024-01-25 19:03:55 +0530

    "update"

--- project/settings.py
+++ project/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'onlineshoping',
+    'supermarkets',
     
 ]
 
@@ -130,8 +130,8 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER='prashanthkurmar143@gmail.com'
-EMAIL_HOST_PASSWORD='kumar@8466'
+EMAIL_HOST_USER=os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD=os.getenv('EMAIL_HOST_PASSWORD')
 # Define the URL pattern for serving static files during development
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- project/urls.py
+++ project/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('onlineshoping.urls')),
+    path('',include('supermarkets.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
