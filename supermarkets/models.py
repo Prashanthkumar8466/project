e7b0a0f1118c778653c0e75818245210050eacac
Prashanthkumar8466 <152079030+Prashanthkumar8466@users.noreply.github.com>
2024-01-29 05:59:58 +0000

    "changes"

--- supermarkets/models.py
+++ supermarkets/models.py
@@ -67,8 +67,8 @@ class Payment(models.Model):
     paid=models.BooleanField(default=False)
 class order(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
-    customer=models.ForeignKey(customer,on_delete=models.CASCADE,default=1)
-    product=models.ForeignKey(product,on_delete=models.CASCADE,default=timezone.now)
+    customer=models.ForeignKey(customer,on_delete=models.CASCADE)
+    product=models.ForeignKey(product,on_delete=models.CASCADE)
     quality=models.PositiveSmallIntegerField(default=1)
     order_date=models.DateTimeField(auto_now_add=True)
     status=models.CharField(max_length=50,choices=STATUS_CHOICE,default='pending')
