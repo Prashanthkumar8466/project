a7fd68e26ff4ed28cd090e9106a97b85d8dbe75b
Prashanth <guntaprashanthkumar@gmail.com>
2024-05-06 12:08:38 +0530

    'changes'

--- realstate/views.py
+++ realstate/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from .models import employee_info
+from django.contrib.auth import login,logout,authenticate
 # Create your views here.
+#@login_required(login_url='login')
 def resume(request):
-    return render(request,'ERP/home.html')
+    employee=employee_info.objects.filter(user=request.user)
+    return render(request,'ERP/home.html',{"employee":employee})
